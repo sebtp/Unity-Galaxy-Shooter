4e13f3a89702fb8308e0e0974191b7ea482e838b
sebtp <sebastianteopopa@gmail.com>
2021-05-14 12:24:52 +0200

    Asteroid and explosion

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private float _rotationSpeed = 17f;
+    [SerializeField]
+    private GameObject _explosion;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Laser"))
+        {
+            Instantiate(_explosion, transform.position, Quaternion.identity);
+            Destroy(GetComponent<CircleCollider2D>());
+            Destroy(gameObject, 1.0f);
+        }
+    }
+}
