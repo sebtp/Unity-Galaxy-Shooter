dea62b40f75545dac03eaa773fe7eadd5eb6d505
sebtp <sebastianteopopa@gmail.com>
2021-07-08 16:33:53 +0200

    Fixed enemy explosion respawning

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -32,7 +32,7 @@ public class Enemy : MonoBehaviour
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
-        if (transform.position.y < -5f)
+        if (transform.position.y < -5f && GetComponent<BoxCollider2D>())
         {
             transform.position = new Vector3(Random.Range(_boundLeft, _boundRight), _boundTop, 0);
         }
